3de6c41b2d76ffa07ecf0e8e94faffa5113e6ec8
Peri <pericles.philippopoulos@gmail.com>
2018-12-08 20:02:08 -0500

    genesisBlock

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,13 @@
 import hashlib
 
-blockChain = [] 
-difficulty = hashlib.sha224(bytes("difficulty", 'utf-8')).hexdigest()
+
 
 
 def BCHash( x ):
 	y = hashlib.sha256(bytes(x, 'utf-8')).hexdigest()
 	return ''.join(format(ord(i), 'b') for i in y)
 
-difficulty = BCHash("difficulty")
+
 
 class Block:
 	def __init__( self, txs, prevHash, nonce ):
@@ -19,6 +18,10 @@ class Block:
 	def __str__(self):
 		return f"txs = {self.txs}, prevHash = {self.prevHash}, nonce = {self.nonce}"
 
+def createBlock( bC, txs):
+	pHash = BCHash( str(bC[-1]) )
+	return Block(txs, pHash, 0)
+
 def mine(block):
 	while(BCHash(str(block)) > difficulty):
 		block.nonce = block.nonce + 1
@@ -26,15 +29,26 @@ def mine(block):
 		print(BCHash(str(block)))
 	blockChain.append(block)
 
-cc = Block( 1, 10, 0 )
-
-mine(cc)
-print(blockChain[0].nonce)
-#print(hash(blockChain[0]))
 
 
-print(str(cc))
 
-print(difficulty<BCHash(str(cc)))
+if __name__ == '__main__':
+	
+	blockChain = [] 
+	difficulty = BCHash("difficulty")
+
+	genBlock = Block(0, 0, 0)
+	blockChain.append(genBlock)
+
+	cc = createBlock( blockChain, 1 )
+
+	mine(cc)
+	print(blockChain[0].nonce)
+	print(blockChain[1].nonce)
+	#print(hash(blockChain[0]))
+	
+	print(str(cc))
+
+	print(difficulty<BCHash(str(cc)))
 
 
